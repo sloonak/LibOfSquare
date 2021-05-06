db4361b991ccc5e9da0bbfefee97064e49a8a164
Akinok <nuqpa1@mail.ru>
2021-05-06 10:53:37 +0300

    Добавьте файлы проекта.

---
+++ LibOfSquare/Square.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LibOfSquare
+{
+    public class Square
+    {
+        public double SquareOfCircleFromRadius(double r)
+        {
+            double square = Math.PI * Math.Pow(r,2);
+            return square;
+        }
+        public double SquareOfCircleFromTriangle(Triangle triangle)
+        {
+            double first_side = triangle.First;
+            double second_side = triangle.Second;
+            double third_side = triangle.Third;
+
+            double p = (first_side + second_side + third_side) / 2;  //полусумма всех сторон
+            double r = (first_side * second_side * third_side) / (4 * Math.Sqrt(p * (p - first_side) * (p - second_side) * (p - third_side)));
+            return SquareOfCircleFromRadius(r);
+        }
+    }
+}
---
+++ LibOfSquare/Triangle.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace LibOfSquare
+{
+    public class Triangle
+    {
+        double first, second, third;
+
+        public Triangle(double first, double second, double third)
+        {
+            First = first;
+            Second = second;
+            Third = third;
+        }
+        public double First
+        {
+            get { return first; }
+            set { first = value < 0 ? -value : value; }
+        }
+        public double Second
+        {
+            get { return second; }
+            set { second = value < 0 ? -value : value; }
+        }
+        public double Third
+        {
+            get { return third; }
+            set { third = value < 0 ? -value : value; }
+        }
+        public string CheckOfTriangle() // подумал что лучше возвращать строковое значение, в случае чего можно поменять на булевое
+        {
+            if ((first * first + second * second == third * third) ||
+                (first * first + third * third == second * second) ||
+                (third * third + second * second == first * first))
+            {
+                return "Это прямоугольный треугольник";
+            }
+            return "Это не прямоугольный треугольник";
+        }
+    }
+}
